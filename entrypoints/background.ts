7a9c73cdbd113cdaa90b9bc6048a199c53fba8c9
Seya <31957516+sglkc@users.noreply.github.com>
2024-12-25 14:50:18 +0700

    feat(content): add markdown transform

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,36 +1,3 @@
-function processPage() {
-  const dom = new DOMParser().parseFromString(document.body.outerHTML, 'text/html')
-  const includeAttributes = ['href', 'alt', 'title', 'role', 'src', 'id', 'name', 'aria']
-  const excludeTags = ['script', 'style', 'link', 'svg', 'iframe', 'noscript']
-
-  const removeUnimportantAttributes = (element: Element) => {
-    const attributes = element.attributes
-
-    for (let i = attributes.length - 1; i >= 0; i--) {
-      const attribute = attributes[i]
-
-      if (!includeAttributes.includes(attribute.name)) {
-        element.removeAttribute(attribute.name)
-      }
-    }
-  }
-
-  dom.querySelectorAll(excludeTags.join()).forEach((elm) => elm.remove())
-  dom.querySelectorAll('*').forEach(removeUnimportantAttributes)
-
-  for (const element of dom.body.children) {
-    for (const child of element.children) {
-      if (!child.childNodes.length || !child.textContent?.trim()) {
-        element.remove()
-        continue
-      }
-      dom.body.appendChild(child)
-    }
-  }
-
-  return dom.body.outerHTML
-}
-
 export default defineBackground(() => {
   chrome.runtime.onMessage.addListener(async (msg) => {
     if (msg !== 'process') return
@@ -41,19 +8,21 @@ export default defineBackground(() => {
 
     const [result] = await chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: processPage
+      files: ['content-scripts/markdown.js']
     })
 
-    console.log(result.result)
+    const md = result.result
 
-    const res = await fetch('http://localhost:5000/minifier', {
-      method: 'post',
-      body: result.result
-    })
+    console.log(md)
 
-    const html = await res.text()
-
-    console.log(html)
+    // const res = await fetch('http://localhost:5000/minifier', {
+    //   method: 'post',
+    //   body: result.result
+    // })
+    //
+    // const html = await res.text()
+    //
+    // console.log(html)
   })
 
   console.log('Hello background!', { id: browser.runtime.id })
---
+++ entrypoints/markdown.content.ts
@@ -0,0 +1,18 @@
+import { ConversionOptions, convertHtmlToMarkdown } from 'dom-to-semantic-markdown'
+
+export default defineContentScript({
+  matches: ['<all_urls>'],
+  registration: 'runtime',
+  main: () => convertHtmlToMarkdown(
+    document.documentElement.outerHTML,
+    {}
+  )
+    .replace(/---\n\n---\n|\n{3,}/g, '\n')
+    .split('\n')
+    .filter((line, index, array) => !(
+      line.trim() === '' &&
+        (index === 0 || array[index - 1].trim() === '') &&
+        (index === array.length - 1 || array[index + 1].trim() === '')
+    ))
+    .join('\n')
+})
